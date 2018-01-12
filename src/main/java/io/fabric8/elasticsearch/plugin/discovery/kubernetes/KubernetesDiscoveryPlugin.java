50a360b36c6f365fc10967ed25015fcbeb4e6366
Artyom Gabeev <artyom.gabeev@gmail.com>
2018-01-12 19:01:45 +0300

    Upgrade to Elasticsearch 6.1.1

--- src/main/java/io/fabric8/elasticsearch/plugin/discovery/kubernetes/KubernetesDiscoveryPlugin.java
+++ src/main/java/io/fabric8/elasticsearch/plugin/discovery/kubernetes/KubernetesDiscoveryPlugin.java
@@ -21,11 +21,14 @@ import io.fabric8.elasticsearch.discovery.kubernetes.KubernetesUnicastHostsProvi
 import org.apache.logging.log4j.Logger;
 import org.apache.lucene.util.SetOnce;
 import org.elasticsearch.SpecialPermission;
-import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.cluster.routing.allocation.AllocationService;
+import org.elasticsearch.cluster.service.ClusterApplier;
+import org.elasticsearch.cluster.service.MasterService;
+import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.logging.DeprecationLogger;
 import org.elasticsearch.common.logging.Loggers;
-import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.network.NetworkService;
+import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.discovery.Discovery;
@@ -66,11 +69,12 @@ public class KubernetesDiscoveryPlugin extends Plugin implements DiscoveryPlugin
   }
 
   @Override
-  public Map<String, Supplier<Discovery>> getDiscoveryTypes(ThreadPool threadPool, TransportService transportService,
-                                                            NamedWriteableRegistry namedWriteableRegistry, ClusterService clusterService, UnicastHostsProvider hostsProvider) {
-    // this is for backcompat with pre 5.1, where users would set discovery.type to use ec2 hosts provider
+  public Map<String, Supplier<Discovery>> getDiscoveryTypes(
+       ThreadPool threadPool, TransportService transportService, NamedWriteableRegistry namedWriteableRegistry, MasterService masterService, ClusterApplier clusterApplier, ClusterSettings clusterSettings, UnicastHostsProvider hostsProvider,
+       AllocationService allocationService) {
     return Collections.singletonMap(KUBERNETES, () ->
-      new ZenDiscovery(settings, threadPool, transportService, namedWriteableRegistry, clusterService, hostsProvider));
+      new ZenDiscovery(settings, threadPool, transportService, namedWriteableRegistry, masterService, clusterApplier, clusterSettings, hostsProvider, allocationService, Arrays.asList(this.getJoinValidator()))
+    );
   }
 
   @Override
